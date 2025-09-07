88e0b5cfa375ae3b2a691cd3b0cd331b31d150cc
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2025-09-07 17:11:35 +0200

    Logic: add initial state

--- Firmware/STM32/Core/Inc/logic.h
+++ Firmware/STM32/Core/Inc/logic.h
@@ -8,27 +8,9 @@
 #ifndef INC_LOGIC_H_
 #define INC_LOGIC_H_
 
-#include "main.h"
-#include "lcd1602.h"
-#include "keyboard.h"
-#include <string.h>
-#include <stdlib.h>
-
 typedef enum {
 	STATE_INIT,
-	STATE_REG_TYPE,
-	STATE_BANG_BANG_TEMP_CONF,
-	STATE_BANG_BANG_OFFSET_CONF,
-	STATE_PID_CONF
+	STATE_CONTROL_TYPE,
 }SystemState;
-
-void state_machine_run(void); // main loop for state machine
-void change_state(SystemState newState); // move between states in a state machine
-
-// State handlers
-void state_init(void);
-void state_reg_type(void);
-void state_bang_bang_temp_conf(void);
-void state_bang_bang_offset_conf(void);
 
 #endif /* INC_LOGIC_H_ */
--- Firmware/STM32/Core/Src/logic.c
+++ Firmware/STM32/Core/Src/logic.c
@@ -15,8 +15,6 @@
 
 static SystemState currentState = STATE_INIT;
 
-static float finalTemperature = 0;
-
 void changeState(SystemState newState)
 {
 	currentState = newState;
@@ -24,6 +22,42 @@ void changeState(SystemState newState)
 
 void stateMachine(void)
 {
-	LCD_SetCursor(0, 0);
-	LCD_SendString("State Machine");
+	switch (currentState)
+	{
+		case STATE_INIT: stateInitHandler(); break;
+		default: break;
+	}
+}
+
+void stateInitHandler(void)
+{
+	static uint32_t lastBlinkTime = 0;
+	static uint8_t textVisible = 1;
+
+	// Print welcome text in the middle of the first line
+	LCD_SetCursor(0, 2);
+	LCD_SendString("TEMP CONTROL");
+
+	// Blinking "CLICK START" text at 1Hz
+	if (HAL_GetTick() - lastBlinkTime >= 500)
+	{
+		lastBlinkTime = HAL_GetTick();
+		textVisible = !textVisible;
+	}
+
+	LCD_SetCursor(1, 3);
+	if (textVisible)
+	{
+		LCD_SendString("CLICK START");
+	}
+	else
+	{
+		LCD_SendString("           ");
+	}
+
+	if (HAL_GPIO_ReadPin(BTN1_GPIO_Port, BTN1_Pin) == 1)
+	{
+		changeState(STATE_CONTROL_TYPE);
+		LCD_Clear();
+	}
 }
